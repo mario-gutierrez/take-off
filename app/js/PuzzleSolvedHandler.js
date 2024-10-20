0381e6a5f1a55ed9adaaee0ee19b46487af3622c
Mario Gutierrez <mgutierrez1@logitech.com>
2024-10-20 16:45:24 +0200

    Remove unused statistics code

--- app/js/PuzzleSolvedHandler.js
+++ app/js/PuzzleSolvedHandler.js
@@ -43,99 +43,32 @@ class PuzzleSolvedHandler {
 
         this.puzzleStats.totalTime.minutes += this.puzzle.timeToSolveMinutes;
         this.puzzleStats.totalTime.seconds += this.puzzle.timeToSolveSeconds;
-
-
-        if (this.puzzleStats.bestTime.minutes * 60 + this.puzzleStats.bestTime.seconds > this.puzzle.timeToSolveMinutes * 60 + this.puzzle.timeToSolveSeconds) {
-            this.puzzleStats.bestTime.minutes = this.puzzle.timeToSolveMinutes;
-            this.puzzleStats.bestTime.seconds = this.puzzle.timeToSolveSeconds;
-            isNewTimeRecord = true;
-        }
-
-        var totalTimeInSeconds = this.puzzleStats.totalTime.minutes * 60 + this.puzzleStats.totalTime.seconds;
-
-        var avgTime = Math.round((totalTimeInSeconds / this.puzzleStats.timesSolved) * 100) / 100;
-
-        var avgTimeMinutes = Math.floor(avgTime / 60);
-        var avgTimeSeconds = Math.round((avgTime - avgTimeMinutes * 60) * 100) / 100;
-
-        this.puzzleStats.avgTime.minutes = avgTimeMinutes;
-        this.puzzleStats.avgTime.seconds = avgTimeSeconds;
-
-
-        //calculate score
-        var puzzleParams = this.puzzle.puzzleDefinition;
-        var scoreParams = {
-            t0: 20,
-            t1: 40,
-            t2: 42,
-            ct: 0.5,
-            m0: 20,
-            m1: 40,
-            m2: 46,
-            cm: 1
-        };
-
-        if ("scoreParams" in puzzleParams) {
-            scoreParams = puzzleParams.scoreParams;
-        }
-
-        var timeToSolveInSeconds = this.puzzle.timeToSolveMinutes * 60 + this.puzzle.timeToSolveSeconds;
-        var newTimeScore = scoreParams.t2 - (timeToSolveInSeconds * scoreParams.ct);
-        newTimeScore = newTimeScore < scoreParams.t0 ? scoreParams.t0 : newTimeScore > scoreParams.t1 ? scoreParams.t1 : newTimeScore;
-
-        var newMoveScore = scoreParams.m2 - (this.puzzle.totalMoves * scoreParams.cm);
-        newMoveScore = newMoveScore < scoreParams.m0 ? scoreParams.m0 : newMoveScore > scoreParams.m1 ? scoreParams.m1 : newMoveScore;
-
-        var newScore = newTimeScore * newMoveScore;
-        if (newScore > this.puzzleStats.bestScore) {
-            this.puzzleStats.bestScore = newScore;
-            isNewScore = true;
-        }
-        this.puzzle.timeScorePercent = this.puzzleStats.timeScore / scoreParams.t1;
-        if (newTimeScore > this.puzzleStats.timeScore) {
-            this.puzzleStats.timeScore = newTimeScore;
-            this.puzzle.timeScorePercent = newTimeScore / scoreParams.t1;
-        }
-        this.puzzle.moveScorePercent = this.puzzleStats.moveScore / scoreParams.m1;
-        if (newMoveScore > this.puzzleStats.moveScore) {
-            this.puzzleStats.moveScore = newMoveScore;
-            this.puzzle.moveScorePercent = newMoveScore / scoreParams.m1;
-        }
-        //display data
-        var score = Math.round(newScore);
         var totalMoves = this.puzzle.totalMoves;
         var timeToSolve = this.puzzle.timeToSolveMinutes + "m " + this.puzzle.timeToSolveSeconds + "s";
+        let movesText = document.getElementById("movesToComplete");
+        movesText.innerHTML = totalMoves;
         var div_winner = document.getElementById("puzzleOverlay");
         div_winner.style.pointerEvents = "visible";
-        let movesSpeed = totalMoves / timeToSolveInSeconds;
-        this.FillInStats(timeToSolve, totalMoves, score, isNewTimeRecord, isNewMovesRecord, isNewScore, movesSpeed.toFixed(2));
         div_winner.style.opacity = 0.99;
-        document.getElementById("mainDiv").style.opacity = 0.05;
         div_winner.classList.toggle('show');
 
         if (this.puzzle.totalMoves > 0) {
             //save data
             localStorage.setItem(this.localStorageRegister, JSON.stringify(this.puzzleStats));
         }
+
+        let canDoBetterMsg = document.getElementById("canDoBetterMessage");
+        let winMessage = document.getElementById("winMessage");
+
+        if (totalMoves <= this.puzzle.puzzleDefinition.movesToWin) {
+            winMessage.innerHTML = "YOU WIN";
+            canDoBetterMsg.innerHTML = "";
+        } else {
+            winMessage.innerHTML = "";
+            canDoBetterMsg.innerHTML = "... but you can do better!";
+        }
         return this.puzzleStats;
     }
-
-    FillInStats(time, moves, score, newTimeRecord, newMovesRecord, newScoreRecord, movesSpeed) {
-        try {
-
-            let movesText = document.getElementById("movesToComplete");
-            movesText.innerHTML = moves;
-            //fill in historic stats table
-            document.getElementById("histRecentTime").innerHTML = time + "";
-            document.getElementById("histRecentMoves").innerHTML = moves + "";
-            document.getElementById("histRecentSpeed").innerHTML = movesSpeed + "";
-            document.getElementById("histRecentScore").innerHTML = score + "";
-
-        } catch (e) {
-            console.log(e);
-        }
-    }
-
 }
 
 function storageAvailable(type) {
