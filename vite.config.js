536437b9530cca1163adc3288f47911049392392
Mario Gutierrez <mgutierrez1@logitech.com>
2024-10-24 14:39:02 +0200

    Add nodejs and vite config files

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+import { resolve } from 'path'
+export default {
+    root: 'app/',
+    publicDir: 'app/',
+    base: './',
+    server:
+    {
+        host: true,
+        open: !isCodeSandbox // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        minify: true,
+        outDir: '../dist',
+        emptyOutDir: true,
+        commonjsOptions: { include: [] },
+        sourcemap: true,
+
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+            }
+        }
+
+    },
+    optimizeDeps: {
+        disabled: false,
+    }
+}
