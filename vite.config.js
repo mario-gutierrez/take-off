d9702d039bc35698cd3de01597bf9a992232a710
Mario Gutierrez <mgutierrez1@logitech.com>
2024-10-24 14:46:32 +0200

    Fix path to index.html

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default {
 
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'src/index.html'),
+                main: resolve(__dirname, 'app/index.html'),
             }
         }
 
