10c08c599e0f890757841eaabb3d2ccbb51ee57a
Mario Gutierrez <mgutierrez1@logitech.com>
2024-10-24 19:53:39 +0200

    Remove vite-plugin-pwa from config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 import { resolve } from 'path'
-import { VitePWA } from 'vite-plugin-pwa'
 export default {
     root: 'app/',
     publicDir: 'app/img/',
@@ -25,26 +24,5 @@ export default {
     },
     optimizeDeps: {
         disabled: false,
-    },
-    plugins: [
-        VitePWA({
-            registerType: 'autoUpdate',
-            manifest: {
-
-                name: "Take Off - Quadratis",
-                short_name: "take off",
-                icons: [
-                    {
-                        src: "icons/quadratis192.png",
-                        sizes: "72x72 96x96 192x192",
-                        type: "image/png"
-                    }
-                ],
-                start_url: "/",
-                display: "standalone",
-                background_color: "#111111",
-                theme_color: "#111111"
-            }
-        })
-    ]
+    }
 }
