0c6cb0efb06c4d1b71a62218b67cb56a76334e8c
Mario Gutierrez <mgutierrez1@logitech.com>
2024-10-24 20:24:06 +0200

    Add basic build config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,10 @@ export default {
     root: 'app/',
     publicDir: 'app/img/',
     base: './',
+    build:
+    {
+        minify: true,
+        outDir: '../dist',
+        emptyOutDir: true
+    }
 }
